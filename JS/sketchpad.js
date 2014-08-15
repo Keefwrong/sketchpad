763337fa96c11f4ef7a482e1ee3b369573d41ee8
Keith Wong <keefwrong@gmail.com>
2014-08-15 15:47:05 -0700

    update CSS & JS

--- JS/sketchpad.js
+++ JS/sketchpad.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
-	createGrid(16,16);
+	createGrid(16);
+	initiateInk();
+	$("button").on('click', reset);
+});
+
+function initiateInk(){
 	$(".column").hover(function(){
 		$(this).addClass("red");
 	});
-});
+}
 
 function reset(){
 	$(".container").find("div").removeClass("red");
-	var gridx = +prompt("How many columns do you want?", "16");
-	var gridy = +prompt("How many rows do you want?", "16");
+	var count = +prompt("How big a grid do you want?", "16");
+	$("span").remove();
+	createGrid(count);
+	initiateInk();
 }
-function createGrid(gridx, gridy){
-	for(var i = 1; i < gridy; i++){
+
+function createGrid(count){
+	for(var i = 1; i < count+1; i++){
 		$(".container").append("<span class=row"+i+"></span>");
-		for(var o = 1; o < gridx; o++){
-			$(".container").find(".row"+i).append("<div class=column "+o+"></div>");
+		for(var o = 1; o < count+1; o++){
+			$(".container").find(".row"+i).append("<div class=\"column "+o+"\"></div>");
 		}
 	}
+	var newContainerSize = 960 - (count*2);
+	var newDimension = newContainerSize/count + "px";
+	$('.container').find('div').css({"height":newDimension,
+									"width":newDimension});
 }
